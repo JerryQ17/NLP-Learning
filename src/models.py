9df1c6cd6bf12463918ce1250e93ac0c55c0b7e6
JerryQ <1397200108@qq.com>
2023-07-10 10:46:30 +0800

    添加了nn训练中断自动保存功能

--- src/models.py
+++ src/models.py
@@ -12,3 +12,10 @@ class GridResult(BaseModel):
     g_min: float = Field(..., title='核函数参数')
     g_max: float = Field(..., title='核函数参数')
     rate: float = Field(..., title='准确率')
+
+
+class NNTrainingState(BaseModel):
+    current_epoch: int = Field(0, title='当前训练轮次')
+    total_epoch: int = Field(0, title='总训练轮次')
+    model_state_dict: dict = Field({}, title='模型参数')
+    optimizer_state_dict: dict = Field({}, title='优化器参数')
