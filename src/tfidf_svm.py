09e745cfec3ffeed070d8cbd9b5169853bcebf10
JerryQ <1397200108@qq.com>
2023-08-17 16:57:13 +0800

    修复了tfidf_svm.py中的一些bug

--- src/tfidf_svm.py
+++ src/tfidf_svm.py
@@ -9,7 +9,7 @@ def tfidf_svm():
     _logger = logging.getLogger(__name__)
     start = time.time()
 
-    dataset = IMDBDataset(r'..\dataset\IMDB Dataset.csv')
+    dataset = IMDBDataset(r'.\dataset\IMDB Dataset.csv')
     _logger.info(f'加载数据集耗时{time.time() - start}秒')
 
     converter = Converter(dataset, processes=15)
@@ -19,7 +19,7 @@ def tfidf_svm():
     trainer = Trainer(tfidf_dataset=dataset, svm_train_path=path)
     _logger.info(f'初始化训练器耗时{time.time() - start}秒')
 
-    results = trainer.svm.grid(enable_logging=True, detailed=True)
+    results = trainer.svm.grid(detailed=True)
     _logger.info(f'网格搜索耗时{time.time() - start}秒')
 
     with open('results.pkl', 'wb') as f:
@@ -31,5 +31,5 @@ def tfidf_svm():
         trainer.svm.train(cost=result.c_min, gamma=result.g_min)
         _logger.info(f"训练模型{i}耗时{time.time() - start}秒")
 
-        savepath = trainer.svm.save(fr'..\svm\data\svm_model{i}_c_{result.c_min}_g_{result.g_min}.model')
+        savepath = trainer.svm.save(fr'.\svm\data\svm_model{i}_c_{result.c_min}_g_{result.g_min}.model')
         _logger.info(f"保存模型{i}耗时{time.time() - start}秒, 保存于{savepath}")
